f33e01e1bbe46f1620dc7d1be519667c6d8a5183
daniel <daniel@192.168.124.14>
2019-10-15 21:05:32 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Description: file content
+ * @Author: Daniel
+ * @LastEditors: Daniel
+ * @Date: 2019-10-15 11:23:07
+ * @LastEditTime: 2019-10-15 12:20:02
+ */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/*
+ * @Description: file content
+ * @Author: Daniel
+ * @LastEditors: Daniel
+ * @Date: 2019-10-15 11:23:07
+ * @LastEditTime: 2019-10-15 11:23:07
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
